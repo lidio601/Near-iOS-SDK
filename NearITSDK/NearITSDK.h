9cacf6ab6d7788648197b0b949e16be13738a5de
Francesco Leoni <francescoleoni@hotmail.com>
2017-03-14 12:14:08 +0100

    Initial Commit

---
+++ NearITSDK/NearITSDK.h
@@ -0,0 +1,19 @@
+//
+//  NearITSDK.h
+//  NearITSDK
+//
+//  Created by Francesco Leoni on 14/03/17.
+//  Copyright © 2017 NearIT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NearITSDK.
+FOUNDATION_EXPORT double NearITSDKVersionNumber;
+
+//! Project version string for NearITSDK.
+FOUNDATION_EXPORT const unsigned char NearITSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NearITSDK/PublicHeader.h>
+
+
