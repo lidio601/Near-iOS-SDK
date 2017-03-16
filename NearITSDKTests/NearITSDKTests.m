99d975bacbd2f8771337a88583b2392399b51597
Francesco Leoni <francesco@nearit.com>
2017-03-16 10:18:40 +0100

    Simple reflection test, may be useful in future

--- NearITSDKTests/NearITSDKTests.m
+++ NearITSDKTests/NearITSDKTests.m
@@ -7,6 +7,9 @@
 //
 
 #import <XCTest/XCTest.h>
+#import "NITJSONAPI.h"
+
+@import ObjectiveC.runtime;
 
 @interface NearITSDKTests : XCTestCase
 
@@ -27,6 +30,21 @@
 - (void)testExample {
     // This is an example of a functional test case.
     // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testReflection {
+    unsigned int outCount;
+    objc_property_t *properties = class_copyPropertyList([NITJSONAPI class], &outCount);
+    for(int i = 0; i < outCount; i++) {
+        objc_property_t property = properties[i];
+        const char *propName = property_getName(property);
+        if(propName) {
+            //const char *propType = getPropertyType(property);
+            NSString *propertyName = [NSString stringWithCString:propName
+                                                        encoding:[NSString defaultCStringEncoding]];
+            NSLog(@"%@", propertyName);
+        }
+    }
 }
 
 - (void)testPerformanceExample {
