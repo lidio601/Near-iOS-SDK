8771ece55dc108f9b827f0435065e23053f7aa45
Francesco Leoni <francesco@nearit.com>
2017-06-27 10:52:58 +0200

    Json unzip test

--- NearITSDK/NSData+Zip.h
+++ NearITSDK/NSData+Zip.h
@@ -8,6 +8,17 @@
 
 #import <Foundation/Foundation.h>
 
+void *NewBase64Decode(
+                      const char *inputBuffer,
+                      size_t length,
+                      size_t *outputLength);
+
+char *NewBase64Encode(
+                      const void *inputBuffer,
+                      size_t length,
+                      bool separateLines,
+                      size_t *outputLength);
+
 @interface NSData (NSDataZip)
 
 // ZLIB
@@ -17,5 +28,9 @@
 // GZIP
 - (NSData *) gzipInflate;
 - (NSData *) gzipDeflate;
+
+// BASE64
+
++ (NSData *)dataFromBase64String:(NSString *)aString;
 
 @end
--- NearITSDKTests/NITZipTest.m
+++ NearITSDKTests/NITZipTest.m
@@ -34,4 +34,22 @@
     XCTAssertTrue([unzippedString isEqualToString:@"Hello World!"]);
 }
 
+- (void)testJsonZipAndUnzip {
+    NSString *json = @"{\"app_id\":\"aa3ffdd3-8111-438e-b55d-d952d5cd6f20\",\"content\":{\"ok\":true},\"owner_id\":\"678eed27-5890-45f9-8214-fa5a4ba584ae\",\"created_at\":\"2017-06-22T16:56:19.891518Z\",\"updated_at\":\"2017-06-22T16:56:19.891518Z\"}";
+    NSData *jsonData = [json dataUsingEncoding:NSUTF8StringEncoding];
+    NSData *zipData = [jsonData zlibDeflate];
+    NSData *unzipped = [zipData zlibInflate];
+    NSString *unzippedJson = [[NSString alloc] initWithData:unzipped encoding:NSUTF8StringEncoding];
+    XCTAssertTrue([unzippedJson isEqualToString:json]);
+}
+
+- (void)testJsonUnzip {
+    NSString *unzippedJsonSample = @"{\"app_id\":\"aa3ffdd3-8111-438e-b55d-d952d5cd6f20\",\"content\":{\"ok\":true},\"owner_id\":\"678eed27-5890-45f9-8214-fa5a4ba584ae\",\"created_at\":\"2017-06-22T16:56:19.891518Z\",\"updated_at\":\"2017-06-22T16:56:19.891518Z\"}";
+    NSString *jsonZip = @"eJyNzMEKgzAMANB/ydmMpja17XfstItEk8IYqIiyg/jvc+wHdn+8A2RZ+qdC\nAZG2VtUWExFhaJPhwKyomb3yqLF6Bw2M87TZtEE5YH5B2dbdzgbm92Tr74ld\nMlPfIafsMHDNmDwFrMISBuEUxL7ParKZ9nJV4B116CJ6f6dYOBbKt5SJKT0u\nui/6Hz0/JMc4wg==";
+    NSData *zipData = [NSData dataFromBase64String:jsonZip];
+    NSData *unzipped = [zipData zlibInflate];
+    NSString *unzippedJson = [[NSString alloc] initWithData:unzipped encoding:NSUTF8StringEncoding];
+    XCTAssertTrue([unzippedJson isEqualToString:unzippedJsonSample]);
+}
+
 @end
