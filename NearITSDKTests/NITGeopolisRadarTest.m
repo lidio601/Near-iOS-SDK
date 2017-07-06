84819c3d0d4b3ee05246691dadfc1591ddc91855
Francesco Leoni <francesco@nearit.com>
2017-07-06 09:48:25 +0200

    More tests for region Inside / Outside

--- NearITSDKTests/NITGeopolisRadarTest.m
+++ NearITSDKTests/NITGeopolisRadarTest.m
@@ -19,6 +19,10 @@
 #import "NITStubGeopolisRadar.h"
 #import "NITFakeLocationManager.h"
 
+#define ID_R1 @"R1"
+#define ID_R2 @"R2"
+#define ID_A1 @"A1"
+
 @interface NITGeopolisRadarTest : NITTestCase
 
 @property (nonatomic, strong) id<NITGeopolisRadarDelegate> delegate;
@@ -124,27 +128,166 @@
 
 // MARK: - Location Manager
 
-- (void)testSimpleGeofence {
+- (void)testSimpleGeofenceDidDetermineInside {
     NITStubGeopolisRadar *radar = [[NITStubGeopolisRadar alloc] initWithDelegate:self.delegate nodesManager:self.nodesManagerOne locationManager:self.locationManager];
-    CLRegion *region = mock([CLRegion class]);
-    [given([region identifier]) willReturn:@"R1"];
+    CLRegion *region = [self makeMockRegionWithIdentifier:ID_R1];
     
-    NITNode *mon1 = [[NITGeofenceNode alloc] init];
-    mon1.identifier = @"R1";
-    [given([self.nodesManagerOne nodeWithID:@"R1"]) willReturn:mon1];
-    [given([self.nodesManagerOne monitoredNodesOnEnterWithId:@"R1"]) willReturn:@[mon1]];
-    [given([self.nodesManagerOne rangedNodesOnEnterWithId:@"R1"]) willReturn:nil];
+    NITNode *mon1 = mock([NITGeofenceNode class]);
+    [given(mon1.ID) willReturn:ID_R1];
+    [given([mon1 createRegion]) willReturn:[self makeMockRegionWithIdentifier:ID_R1]];
+    [given([self.nodesManagerOne nodeWithID:ID_R1]) willReturn:mon1];
+    [given([self.nodesManagerOne monitoredNodesOnEnterWithId:ID_R1]) willReturn:@[mon1]];
+    [given([self.nodesManagerOne rangedNodesOnEnterWithId:ID_R1]) willReturn:nil];
     [radar simulateDidDetermineStateWithRegion:region state:CLRegionStateInside];
     
-    [verifyCount(self.nodesManagerOne, times(1)) monitoredNodesOnEnterWithId:@"R1"];
-    [verifyCount(self.nodesManagerOne, times(1)) rangedNodesOnEnterWithId:@"R1"];
+    [verifyCount(self.nodesManagerOne, times(1)) monitoredNodesOnEnterWithId:ID_R1];
+    [verifyCount(self.nodesManagerOne, times(1)) rangedNodesOnEnterWithId:ID_R1];
+    [verifyCount(self.nodesManagerOne, never()) monitoredNodesOnExitWithId:ID_R1];
+    [verifyCount(self.nodesManagerOne, never()) rangedNodesOnExitWithId:ID_R1];
     [verifyCount(self.locationManager, times(1)) requestStateForRegion:anything()];
     [verifyCount(self.locationManager, times(1)) startMonitoringForRegion:anything()];
+    [verifyCount(self.locationManager, never()) stopMonitoringForRegion:anything()];
+    [verifyCount(self.locationManager, never()) startRangingBeaconsInRegion:anything()];
+    [verifyCount(self.locationManager, never()) stopMonitoringForRegion:anything()];
+    
+    // Should trigger "enter place" because is the first determine state
     [verifyCount(self.delegate, times(1)) geopolisRadar:sameInstance(radar) didTriggerWithNode:sameInstance(mon1) event:NITRegionEventEnterPlace];
     [verifyCount(self.delegate, never()) geopolisRadar:sameInstance(radar) didTriggerWithNode:anything() event:NITRegionEventEnterArea];
     [verifyCount(self.delegate, never()) geopolisRadar:anything() didTriggerWithNode:anything() event:NITRegionEventEnterArea];
     [verifyCount(self.delegate, never()) geopolisRadar:anything() didTriggerWithNode:anything() event:NITRegionEventLeavePlace];
     [verifyCount(self.delegate, never()) geopolisRadar:anything() didTriggerWithNode:anything() event:NITRegionEventLeaveArea];
+    
+    // The second determine state on the same region should not call any trigger
+    [radar simulateDidDetermineStateWithRegion:region state:CLRegionStateInside];
+    [verifyCount(self.delegate, never()) geopolisRadar:sameInstance(radar) didTriggerWithNode:sameInstance(mon1) event:NITRegionEventEnterPlace];
+    [verifyCount(self.delegate, never()) geopolisRadar:sameInstance(radar) didTriggerWithNode:anything() event:NITRegionEventEnterArea];
+    [verifyCount(self.delegate, never()) geopolisRadar:anything() didTriggerWithNode:anything() event:NITRegionEventEnterArea];
+    [verifyCount(self.delegate, never()) geopolisRadar:anything() didTriggerWithNode:anything() event:NITRegionEventLeavePlace];
+    [verifyCount(self.delegate, never()) geopolisRadar:anything() didTriggerWithNode:anything() event:NITRegionEventLeaveArea];
+}
+
+- (void)testSimpleAreaDidDetermineInside {
+    NITStubGeopolisRadar *radar = [[NITStubGeopolisRadar alloc] initWithDelegate:self.delegate nodesManager:self.nodesManagerOne locationManager:self.locationManager];
+    CLRegion *region = [self makeMockRegionWithIdentifier:ID_A1];
+    
+    NITNode *bNode1 = mock([NITBeaconNode class]);
+    [given(bNode1.ID) willReturn:ID_A1];
+    [given([bNode1 createRegion]) willReturn:[self makeMockRegionWithIdentifier:ID_A1]];
+    [given([self.nodesManagerOne nodeWithID:ID_A1]) willReturn:bNode1];
+    [given([self.nodesManagerOne monitoredNodesOnEnterWithId:ID_A1]) willReturn:@[bNode1]];
+    [given([self.nodesManagerOne rangedNodesOnEnterWithId:ID_A1]) willReturn:nil];
+    [radar simulateDidDetermineStateWithRegion:region state:CLRegionStateInside];
+    
+    [verifyCount(self.nodesManagerOne, times(1)) monitoredNodesOnEnterWithId:ID_A1];
+    [verifyCount(self.nodesManagerOne, times(1)) rangedNodesOnEnterWithId:ID_A1];
+    [verifyCount(self.nodesManagerOne, never()) monitoredNodesOnExitWithId:ID_A1];
+    [verifyCount(self.nodesManagerOne, never()) rangedNodesOnExitWithId:ID_A1];
+    [verifyCount(self.locationManager, times(1)) requestStateForRegion:anything()];
+    [verifyCount(self.locationManager, times(1)) startMonitoringForRegion:anything()];
+    [verifyCount(self.locationManager, never()) stopMonitoringForRegion:anything()];
+    [verifyCount(self.locationManager, never()) startRangingBeaconsInRegion:anything()];
+    [verifyCount(self.locationManager, never()) stopMonitoringForRegion:anything()];
+    
+    // Should trigger "enter area" because is the first determine state
+    [verifyCount(self.delegate, times(1)) geopolisRadar:sameInstance(radar) didTriggerWithNode:sameInstance(bNode1) event:NITRegionEventEnterArea];
+    [verifyCount(self.delegate, never()) geopolisRadar:sameInstance(radar) didTriggerWithNode:anything() event:NITRegionEventEnterPlace];
+    [verifyCount(self.delegate, never()) geopolisRadar:anything() didTriggerWithNode:anything() event:NITRegionEventEnterArea];
+    [verifyCount(self.delegate, never()) geopolisRadar:anything() didTriggerWithNode:anything() event:NITRegionEventLeavePlace];
+    [verifyCount(self.delegate, never()) geopolisRadar:anything() didTriggerWithNode:anything() event:NITRegionEventLeaveArea];
+    
+    // The second determine state on the same region should not call any trigger
+    [radar simulateDidDetermineStateWithRegion:region state:CLRegionStateInside];
+    [verifyCount(self.delegate, never()) geopolisRadar:sameInstance(radar) didTriggerWithNode:anything() event:NITRegionEventEnterPlace];
+    [verifyCount(self.delegate, never()) geopolisRadar:sameInstance(radar) didTriggerWithNode:anything() event:NITRegionEventEnterArea];
+    [verifyCount(self.delegate, never()) geopolisRadar:anything() didTriggerWithNode:anything() event:NITRegionEventEnterArea];
+    [verifyCount(self.delegate, never()) geopolisRadar:anything() didTriggerWithNode:anything() event:NITRegionEventLeavePlace];
+    [verifyCount(self.delegate, never()) geopolisRadar:anything() didTriggerWithNode:anything() event:NITRegionEventLeaveArea];
+}
+
+- (void)testSimpleGeofenceDidDetermineOutside {
+    NITStubGeopolisRadar *radar = [[NITStubGeopolisRadar alloc] initWithDelegate:self.delegate nodesManager:self.nodesManagerOne locationManager:self.locationManager];
+    CLRegion *region = [self makeMockRegionWithIdentifier:ID_R1];
+    
+    NITNode *mon1 = mock([NITGeofenceNode class]);
+    [given(mon1.ID) willReturn:ID_R1];
+    [given([mon1 createRegion]) willReturn:[self makeMockRegionWithIdentifier:ID_R1]];
+    [given([self.nodesManagerOne nodeWithID:ID_R1]) willReturn:mon1];
+    [given([self.nodesManagerOne monitoredNodesOnExitWithId:ID_R1]) willReturn:@[mon1]];
+    [given([self.nodesManagerOne rangedNodesOnExitWithId:ID_R1]) willReturn:nil];
+    [given([self.locationManager monitoredRegions]) willReturn:nil];
+    
+    // Test region outside without monitored regions, should "never" call
+    [radar simulateDidDetermineStateWithRegion:region state:CLRegionStateOutside];
+    
+    [verifyCount(self.nodesManagerOne, never()) monitoredNodesOnExitWithId:ID_R1];
+    [verifyCount(self.nodesManagerOne, never()) rangedNodesOnExitWithId:ID_R1];
+    [verifyCount(self.locationManager, never()) requestStateForRegion:anything()];
+    [verifyCount(self.locationManager, never()) startMonitoringForRegion:anything()];
+    [verifyCount(self.locationManager, never()) stopMonitoringForRegion:anything()];
+    [verifyCount(self.locationManager, never()) startRangingBeaconsInRegion:anything()];
+    [verifyCount(self.locationManager, never()) stopMonitoringForRegion:anything()];
+    
+    // Did determine Outside should not trigger anything
+    [verifyCount(self.delegate, never()) geopolisRadar:sameInstance(radar) didTriggerWithNode:sameInstance(mon1) event:NITRegionEventLeavePlace];
+    [verifyCount(self.delegate, never()) geopolisRadar:sameInstance(radar) didTriggerWithNode:anything() event:NITRegionEventEnterArea];
+    [verifyCount(self.delegate, never()) geopolisRadar:anything() didTriggerWithNode:anything() event:NITRegionEventEnterArea];
+    [verifyCount(self.delegate, never()) geopolisRadar:anything() didTriggerWithNode:anything() event:NITRegionEventEnterPlace];
+    [verifyCount(self.delegate, never()) geopolisRadar:anything() didTriggerWithNode:anything() event:NITRegionEventLeaveArea];
+    
+    NSSet<CLRegion*> *monitoredRegions = [[NSSet alloc] initWithObjects:region, nil];
+    [given([self.locationManager monitoredRegions]) willReturn:monitoredRegions];
+    [radar simulateDidDetermineStateWithRegion:region state:CLRegionStateOutside];
+    
+    [verifyCount(self.nodesManagerOne, times(1)) monitoredNodesOnExitWithId:ID_R1];
+    [verifyCount(self.nodesManagerOne, times(1)) rangedNodesOnExitWithId:ID_R1];
+    [verifyCount(self.nodesManagerOne, never()) monitoredNodesOnEnterWithId:anything()];
+    [verifyCount(self.nodesManagerOne, never()) rangedNodesOnEnterWithId:anything()];
+    
+    // Should not call locationManager monitoring because monitoredRegions there is the same node returned by "monitoredNodesOnExitWithId"
+    [verifyCount(self.locationManager, never()) requestStateForRegion:anything()];
+    [verifyCount(self.locationManager, never()) startMonitoringForRegion:anything()];
+    [verifyCount(self.locationManager, never()) stopMonitoringForRegion:anything()];
+    [verifyCount(self.locationManager, never()) startRangingBeaconsInRegion:anything()];
+    [verifyCount(self.locationManager, never()) stopMonitoringForRegion:anything()];
+    
+    // Did determine Outside should not trigger anything
+    [verifyCount(self.delegate, never()) geopolisRadar:sameInstance(radar) didTriggerWithNode:sameInstance(mon1) event:NITRegionEventLeavePlace];
+    [verifyCount(self.delegate, never()) geopolisRadar:sameInstance(radar) didTriggerWithNode:anything() event:NITRegionEventEnterArea];
+    [verifyCount(self.delegate, never()) geopolisRadar:anything() didTriggerWithNode:anything() event:NITRegionEventEnterArea];
+    [verifyCount(self.delegate, never()) geopolisRadar:anything() didTriggerWithNode:anything() event:NITRegionEventEnterPlace];
+    [verifyCount(self.delegate, never()) geopolisRadar:anything() didTriggerWithNode:anything() event:NITRegionEventLeaveArea];
+    
+    CLRegion *region2 = [self makeMockRegionWithIdentifier:ID_R2];
+    monitoredRegions = [[NSSet alloc] initWithObjects:region, region2, nil];
+    [given([self.locationManager monitoredRegions]) willReturn:monitoredRegions];
+    [radar simulateDidDetermineStateWithRegion:region state:CLRegionStateOutside];
+    
+    [verifyCount(self.nodesManagerOne, times(1)) monitoredNodesOnExitWithId:ID_R1];
+    [verifyCount(self.nodesManagerOne, times(1)) rangedNodesOnExitWithId:ID_R1];
+    [verifyCount(self.nodesManagerOne, never()) monitoredNodesOnEnterWithId:anything()];
+    [verifyCount(self.nodesManagerOne, never()) rangedNodesOnEnterWithId:anything()];
+    
+    // Should call locationManager stop monitoring for region2
+    [verifyCount(self.locationManager, never()) requestStateForRegion:anything()];
+    [verifyCount(self.locationManager, never()) startMonitoringForRegion:anything()];
+    [verifyCount(self.locationManager, times(1)) stopMonitoringForRegion:anything()];
+    [verifyCount(self.locationManager, never()) startRangingBeaconsInRegion:anything()];
+    [verifyCount(self.locationManager, never()) stopMonitoringForRegion:anything()];
+    
+    // Did determine Outside should not trigger anything
+    [verifyCount(self.delegate, never()) geopolisRadar:sameInstance(radar) didTriggerWithNode:sameInstance(mon1) event:NITRegionEventLeavePlace];
+    [verifyCount(self.delegate, never()) geopolisRadar:sameInstance(radar) didTriggerWithNode:anything() event:NITRegionEventEnterArea];
+    [verifyCount(self.delegate, never()) geopolisRadar:anything() didTriggerWithNode:anything() event:NITRegionEventEnterArea];
+    [verifyCount(self.delegate, never()) geopolisRadar:anything() didTriggerWithNode:anything() event:NITRegionEventEnterPlace];
+    [verifyCount(self.delegate, never()) geopolisRadar:anything() didTriggerWithNode:anything() event:NITRegionEventLeaveArea];
+}
+
+// MARK: - Utils
+
+- (CLRegion*)makeMockRegionWithIdentifier:(NSString*)identifier {
+    CLRegion *region = mock([CLRegion class]);
+    [given([region identifier]) willReturn:identifier];
+    return region;
 }
 
 @end
